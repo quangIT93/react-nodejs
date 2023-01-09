7e295804f6b23baf2d3c5d4f8ccb3823d4fe6c41
quangIT93 <quangbk5493@gmail.com>
2023-01-10 02:04:21 +0700

    commit 5

--- client/src/contexts/constants.jsx
+++ client/src/contexts/constants.jsx
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://react-nodejs-server-quangit93.vercel.app"
+    ? "https://react-nodejs-server-quangit93.vercel.app/api"
     : "http://localhost:5000/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
